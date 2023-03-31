5149e18c43f4f2cb7f5bafbdc40ea14df3ca9065
Domaeae <DominikGirrbach@live.de>
2023-03-31 02:35:39 +0200

    +1

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function () {
+
+  // const $btn_all = $('.btn, .bsbtn');
+  // const $btn_darkinfos = $('#btn_getinfos');
+
+  // $btn_darkinfos.click(function(event) {
+  //   alert('XX')
+  //   window.open("https://google.com");
+  // });
+
+
+  // const
+
+});
